939d8cd3ed0ddf294dd9616c1b2013859161e1c1
Seong Yong-ju <sei40kr@gmail.com>
2017-11-18 02:25:28 +0900

    Add build script
    Add build script
    * Add a file `build.sh`.
    * Check requirements to make sandbox environment up.

--- build.sh
+++ build.sh
@@ -3,5 +3,51 @@
 # build.sh
 # @author Seong Yong-ju ( @sei40kr )
 
-env TERM=xterm ./gradlew build
+cat <<EOM
+==> Checking Docker and Docker Compose are set up correctly ...
+
+EOM
+
+if ! command -v docker 1>/dev/null
+then
+  cat 1>&2 <<EOM
+  Error: Docker is not installed.
+
+EOM
+  exit 1
+fi
+
+if ! command -v docker-compose 1>/dev/null
+then
+  cat 1>&2 <<EOM
+  Error: Docker Compose is not installed.
+
+EOM
+  exit 1
+fi
+
+if ! groups "$USER" | grep -qe '\bdocker\b'
+then
+  cat 1>&2 <<EOM
+  Error: You should be a member of group \`docker\`. To resolve this, run this:
+
+    sudo usermod -aG docker "${USERNAME}"
+
+EOM
+  exit 1
+fi
+
+cat <<EOM
+  Everything looks fine!
+
+EOM
+
+cat 1>&2 <<EOM
+==> Running \`docker-compose -f docker-compose-dev.yml up\`
+
+EOM
+
+env \
+  TERM=xterm \
+  docker-compose -f docker-compose-dev.yml up
 
